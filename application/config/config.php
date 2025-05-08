773ccbc3e1537ce08611286692cb12511f0f4164
Shoeb Shaikh <shoeb.shaikh@vervali.com>
2025-05-08 17:19:25 +0530

    extended token expiry from 2hrs to 24hr

--- application/config/config.php
+++ application/config/config.php
@@ -401,7 +401,7 @@ $config['cookie_secure'] = strpos($config['base_url'], 'https') !== false;
 $config['csrf_protection'] = true;
 $config['csrf_token_name'] = 'csrf_token';
 $config['csrf_cookie_name'] = 'csrf_cookie';
-$config['csrf_expire'] = 7200;
+$config['csrf_expire'] = 86400; // 24hrs
 $config['csrf_exclude_uris'] = ['api/v1/.*', 'booking/.*', 'booking_cancellation/.*', 'booking_confirmation/.*'];
 
 /*
